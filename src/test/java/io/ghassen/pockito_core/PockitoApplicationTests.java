611c875308f372ca453b3bea928b4a96954ef5f7
ghassenbrg <ghassen.brg16@gmail.com>
2025-08-15 17:16:25 +0200

    Initialize Pockito core

---
+++ src/test/java/io/ghassen/pockito_core/PockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ghassen.pockito_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
