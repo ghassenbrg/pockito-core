91dce6c580e0acc4657cb489527d525e3284741c
ghassenbrg <ghassen.brg16@gmail.com>
2025-08-18 15:56:53 +0200

    feat: add H2 in-memory database configuration and test profile support

--- src/test/java/io/ghassen/pockito/PockitoApplicationTests.java
+++ src/test/java/io/ghassen/pockito/PockitoApplicationTests.java
@@ -2,8 +2,10 @@ package io.ghassen.pockito;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PockitoApplicationTests {
 
 	@Test
